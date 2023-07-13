eb362ff71c541a7f4042979b23b8dd9bdb129c2d
VladislavMarychev <vladislav.marychev@yandex.ru>
2023-07-13 19:00:08 +0300

    Задача 3 сделана

---
+++ Example2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int Vvod(string text)
+    {
+        Console.WriteLine(text);
+        return Convert.ToInt32(Console.ReadLine());
+
+    }
+
+    int number = Vvod ("Введите число");
+
+    int index = 1;
+    while (index <= number)
+    {
+        Console.WriteLine ($"{index}^3 = {Math.Pow(index, 3)} ");
+        index++;
+    }
